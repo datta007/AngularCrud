5eb9be0b3b34884f025828f4353e09c8582b5f4a
Dattatray Pawar <Dattatray.Pawar@xome.com>
2019-04-18 08:47:32 -0500

    custom data filtering logic

--- src/app/employees/list-employees.component.ts
+++ src/app/employees/list-employees.component.ts
@@ -8,28 +8,46 @@ import { Router } from '@angular/router';
   styleUrls: ['./list-employees.component.css']
 })
 export class ListEmployeesComponent implements OnInit {
+
+  constructor(private employeeService: EmployeeService, private _router: Router) { }
   private employees: Employee[];
   employeeToDisplay: Employee;
-  searchTerm: string;
+  filteredEmployees: Employee[];
+  private _searchTerm: string;
   private arrayIndex = 1;
   private dataFromChild: Employee;
-  constructor(private employeeService: EmployeeService, private _router: Router) { }
+
+  get searchTerm(): string {
+    return this._searchTerm;
+  }
+
+  set searchTerm(value: string) {
+    this._searchTerm = value;
+    this.filteredEmployees = this.getfilteredemployee(value);
+  }
+
+  getfilteredemployee(searchTerm: string) {
+
+    return this.employees.filter(x => x.name.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1);
+  }
 
   ngOnInit() {
     this.employees = this.employeeService.getEmployees();
     this.employeeToDisplay = this.employees[0];
+    this.filteredEmployees = this.employees;
   }
   handleNotify(eventData: Employee) {
     this.dataFromChild = eventData;
   }
 
   onClick(employeeId: number) {
-this._router.navigate(['/employees', employeeId]);
+    this._router.navigate(['/employees', employeeId]);
   }
   changeemployeeName(): void {
-     const newEmployee: Employee[] = Object.assign([], this.employees);
-     newEmployee[0].name = 'Jordan';
-     this.employees = newEmployee;
+    // const newEmployee: Employee[] = Object.assign([], this.employees);
+    this.employees[0].name = 'Jordan';
+    // this.employees = newEmployee;
+    this.filteredEmployees = this.getfilteredemployee(this.searchTerm);
 
   }
   nextEmployee(): void {
